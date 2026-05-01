58a99479eca2e4c7bd4ae9c7d5e71f24a3a6cc43
Muhammad Ridwan Farizqi <mridwan6661@gmail.com>
2026-05-01 20:59:13 +0700

    fix powermenu confirmation

--- config/rofi/scripts/powermenu.sh
+++ config/rofi/scripts/powermenu.sh
@@ -23,72 +23,72 @@ no='No'
 
 # Rofi CMD
 rofi_cmd() {
-  rofi -dmenu \
-    -mesg " Uptime: $uptime" \
-    -theme ~/.config/rofi/applets/powermenu.rasi
+    rofi -dmenu \
+        -mesg " Uptime: $uptime" \
+        -theme ~/.config/rofi/applets/powermenu.rasi
 }
 
 # Confirmation CMD
 confirm_cmd() {
-  rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px;}' \
-    -theme-str 'mainbox {children: [ "message", "listview" ];}' \
-    -theme-str 'listview {columns: 2; lines: 1;}' \
-    -theme-str 'element-text {horizontal-align: 0.5;}' \
-    -theme-str 'textbox {horizontal-align: 0.5;}' \
-    -dmenu \
-    -p 'Confirmation' \
-    -mesg 'Are you Sure?' \
-    -theme ~/.config/rofi/applets/powermenu.rasi
+    rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 250px; height: 230px;}' \
+        -theme-str 'mainbox {children: [ "message", "listview" ];}' \
+        -theme-str 'listview {columns: 1; lines: 1;}' \
+        -theme-str 'element-text {horizontal-align: 0.5;}' \
+        -theme-str 'textbox {horizontal-align: 0.5;}' \
+        -dmenu \
+        -p 'Confirmation' \
+        -mesg 'Are you Sure?' \
+        -theme ~/.config/rofi/applets/powermenu.rasi
 }
 
 # Ask for confirmation
 confirm_exit() {
-  echo -e "$yes\n$no" | confirm_cmd
+    echo -e "$yes\n$no" | confirm_cmd
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-  echo -e "$shutdown\n$reboot\n$lock\n$suspend\n$logout" | rofi_cmd
+    echo -e "$shutdown\n$reboot\n$lock\n$suspend\n$logout" | rofi_cmd
 }
 
 # Execute Command
 run_cmd() {
-  selected="$(confirm_exit)"
-  if [[ "$selected" == "$yes" ]]; then
-    if [[ $1 == '--shutdown' ]]; then
-      systemctl poweroff
-    elif [[ $1 == '--reboot' ]]; then
-      systemctl reboot
-    elif [[ $1 == "--lock" ]]; then
-      swaylock -f
-    elif [[ $1 == '--suspend' ]]; then
-      mpc -q pause
-      amixer set Master mute
-      systemctl suspend
-    elif [[ $1 == '--logout' ]]; then
-      niri msg action quit -s
+    selected="$(confirm_exit)"
+    if [[ "$selected" == "$yes" ]]; then
+        if [[ $1 == '--shutdown' ]]; then
+            systemctl poweroff
+        elif [[ $1 == '--reboot' ]]; then
+            systemctl reboot
+        elif [[ $1 == "--lock" ]]; then
+            swaylock -f
+        elif [[ $1 == '--suspend' ]]; then
+            mpc -q pause
+            amixer set Master mute
+            systemctl suspend
+        elif [[ $1 == '--logout' ]]; then
+            niri msg action quit -s
+        fi
+    else
+        exit 0
     fi
-  else
-    exit 0
-  fi
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
 $shutdown)
-  run_cmd --shutdown
-  ;;
+    run_cmd --shutdown
+    ;;
 $reboot)
-  run_cmd --reboot
-  ;;
+    run_cmd --reboot
+    ;;
 $lock)
-  run_cmd --lock
-  ;;
+    run_cmd --lock
+    ;;
 $suspend)
-  run_cmd --suspend
-  ;;
+    run_cmd --suspend
+    ;;
 $logout)
-  run_cmd --logout
-  ;;
+    run_cmd --logout
+    ;;
 esac
